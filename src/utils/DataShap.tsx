9f92bc32f0ed7079b159b00c0c26af039ea84298
Xiaochi <294188719@qq.com>
2018-10-07 19:49:14 +1000

    create api simulator to provide data

---
+++ src/utils/DataShap.tsx
@@ -0,0 +1,7 @@
+export interface AssosiatedAthelets {
+  ID: string,
+  athlete: string,
+  muscleSoreness: number,
+  sleepQuality: number
+}
+
--- src/utils/apiSimulator.tsx
+++ src/utils/apiSimulator.tsx
@@ -1,1 +1,28 @@
-import * as _DATA from './_DATA.json'
+import * as _ATHLETE_DATA from './_DATA.json';
+import {AssosiatedAthelets} from "./DataShap";
+
+
+function generateUID() {
+  return Math.random().toString(36).substring(2, 15) + Math.random().toString(36).substring(2, 15)
+}
+
+function associateStaticsToAthlete(athletData: any) {
+  const associatesAthletes: AssosiatedAthelets[] = [];
+  for (let i = 0; i < athletData.athlete.length; i++) {
+    associatesAthletes.push({
+      ID: generateUID(),
+      athlete: athletData.athlete[i],
+      muscleSoreness: Number(athletData.muscleSoreness[i]),
+      sleepQuality: Number(athletData.sleepQuality[i])
+    })
+  }
+  return associatesAthletes;
+}
+
+export function _getAthletes() {
+  return new Promise((res, rej) => {
+    setTimeout(() => res({
+      athletes: associateStaticsToAthlete(_ATHLETE_DATA)
+    }), 1000);
+  })
+}
